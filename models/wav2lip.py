59a1f31e9bd80fbe5a921d7de8d235f0e290d0ab
xiedongmingming <986617424@qq.com>
2023-09-24 22:23:32 +0800

    ID: #1234 msg: 优化

--- models/wav2lip.py
+++ models/wav2lip.py
@@ -98,18 +98,22 @@ class Wav2Lip(nn.Module):
         self.audio_encoder = nn.Sequential(
 
             Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+
             Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True),
             Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True),
 
             Conv2d(32, 64, kernel_size=3, stride=(3, 1), padding=1),
+
             Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
             Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
 
             Conv2d(64, 128, kernel_size=3, stride=3, padding=1),
+
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
 
             Conv2d(128, 256, kernel_size=3, stride=(3, 2), padding=1),
+
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
 
             Conv2d(256, 512, kernel_size=3, stride=1, padding=0),
